75ed4235672bd4632d2b98f3b7f3195559c16eb8
Felix N <global>
2024-08-09 00:52:04 -0300

    teste

--- .vscode/conta-banco/src/ContaTerminal.java
+++ .vscode/conta-banco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
+        Scanner leitura = new Scanner(System.in);
+
+        Double saldo = 1300.20;
+
+        System.out.println("Insira seu Nome: ");
+        String nomeCliente = leitura.nextLine();
+
+        System.out.println("Insira a Agência: ");
+        String agencia = leitura.nextLine();
+
+        System.out.println("Insira o Número da Agência: ");
+        Integer numeroAgencia = leitura.nextInt();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco. Sua agência é "
+                + agencia
+                + ", conta " + numeroAgencia + " e seu saldo de R$" + saldo + " já está disponível para saque.");
+
+        leitura.close();
 
     }
 }
